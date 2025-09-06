0d7e84a472c5998365461d4eb970e97b90a660fa
Miller Ossa <miller.ossa@gmail.com>
2025-09-05 21:00:22 -0500

    solucion ejercicio 1 dom js calendario

--- DOM-JS-Calendario/main.js
+++ DOM-JS-Calendario/main.js
@@ -1,1 +1,13 @@
+const anioActual = new Date().getFullYear();
 
+const dateInput = document.getElementById("date-id");
+let fechaSeleccionada = "";
+dateInput.addEventListener("change", function () {
+  fechaSeleccionada = this.value;
+
+  const anioSeleccionado = new Date(fechaSeleccionada).getFullYear();
+  const edad = anioActual - anioSeleccionado;
+
+  const resultadoElemento = document.getElementById("resultado-edad");
+  resultadoElemento.textContent = `Tu edad es: ${edad} años`;
+});
